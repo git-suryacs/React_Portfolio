50a47465aca041cfe8f5566db772b1feb3361f91
git-suryacs <2suryacs@gmail.com>
2022-10-17 18:47:45 +0100

    Work component

---
+++ portfolio-react-app/src/components/Work.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4'>Work</p>
+                <p>Check out some of my recent work</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <span> 
+
+                        </span>
+                        <div>
+                            <a href="/"> 
+                                <button>
+                                    
+                                </button>
+                            </a>
+                            <a href="/"> 
+                                <button>
+
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
