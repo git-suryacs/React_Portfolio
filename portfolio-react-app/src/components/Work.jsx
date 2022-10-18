48487ddf60b6b9a07204ddb507790cd7cff0ba3f
git-suryacs <2suryacs@gmail.com>
2022-10-18 13:46:20 +0100

    Text modifications

--- portfolio-react-app/src/components/Work.jsx
+++ portfolio-react-app/src/components/Work.jsx
@@ -8,7 +8,7 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
-            Work
+            Projects
           </p>
           <p className='py-6'>Check out some of my recent work</p>
         </div>
