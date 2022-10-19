7aba12ff5fff14723bb033d3070128eb181be2ce
git-suryacs <2suryacs@gmail.com>
2022-10-19 11:08:18 +0100

    Skills general update

--- portfolio-react-app/src/components/Skills.jsx
+++ portfolio-react-app/src/components/Skills.jsx
@@ -19,7 +19,7 @@ const Skills = () => {
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-300'>Skills</p>
-                    <p className='py-4'>These are the technologies I've worked with</p>
+                    <p className='py-4'>These are the technologies I've worked with and a lot more</p>
                 </div>
                 {/* Containter for all the icons */}
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
