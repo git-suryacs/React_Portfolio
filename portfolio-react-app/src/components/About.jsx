37b29b1225d8406f7f46cfaf963ebcbeaf7117ca
git-suryacs <2suryacs@gmail.com>
2022-10-19 10:24:25 +0100

    Basic info added

--- portfolio-react-app/src/components/About.jsx
+++ portfolio-react-app/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='max-w-[1000px] mx-auto flex flex-col justify-center items-center w-full h-full'>
             <div className=' w-full grid grid-cols-2 gap-8'> 
                 <div className='sm:text-right pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-300'>About</p>
                 </div>
                 <div>
 
@@ -14,14 +14,11 @@ const About = () => {
                 </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                   <div className='sm:text-right text-4xl font-bold'>
-                    <p>Hi. I am Surya, nice to meet you. Please take a look around</p>
+                    <p>Nice to have you here and please take a look around</p>
                   </div>
                   <div>
                     <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>
+              the lives of those around me. Seeking to use proven technicall skills and manage projects via JIRA to meet business needs.</p>
                   </div>
 
                 </div>
--- portfolio-react-app/src/components/Skills.jsx
+++ portfolio-react-app/src/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
             {/* Container */}
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-300'>Skills</p>
                     <p className='py-4'>These are the technologies I've worked with</p>
                 </div>
                 {/* Containter for all the icons */}
